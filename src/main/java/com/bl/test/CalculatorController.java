b30a49525891cee8c5138b93ca11c6a97a8d128e
bhavesh357 <bkadam357@gmail.com>
2020-06-14 18:45:21 +0530

    first commit

---
+++ src/main/java/com/bl/test/CalculatorController.java
@@ -0,0 +1,17 @@
+package com.bl.test;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class CalculatorController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(){
+        return "welcome";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<head>
+    <title>Calculator</title>
+</head>
+<body>
+<h2>Welcome to Calculator</h2>
+</body>
+</html>
